fa2755e677c4506671b5304d6c79b8133b6396f2
SakuyaYae <darkreimu@outlook.com>
2021-08-18 13:36:48 +0200

    added futionality

---
+++ js.js
@@ -0,0 +1,5 @@
+data = {data:[
+{"managID":1,Title:"NO GAME NO LIFE",Rating:"prefect",number:"1"},
+{"managID":1,Title:"SAO Holow relasation",Rating:"Great",number:"2"}
+]}
+var Anime = document.getElementById()
